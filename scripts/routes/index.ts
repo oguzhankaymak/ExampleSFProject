e388adfed5df2f316099abc70ae58a604b31c55e
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-05-06 08:56:04 +0300

    Add tabbar navigation

--- scripts/routes/index.ts
+++ scripts/routes/index.ts
@@ -1,46 +1,17 @@
-import { NativeRouter, NativeStackRouter, Route } from '@smartface/router';
-import * as Pages from 'pages';
+
+import { NativeRouter } from '@smartface/router';
 import Application from '@smartface/native/application';
+import TabbarRoute from './tabbar';
 
 Application.on(Application.Events.BackButtonPressed, () => {
-    NativeRouter.getActiveRouter()?.goBack();
+  NativeRouter.getActiveRouter()?.goBack();
 });
 
 const router = NativeRouter.of({
   path: '/',
   isRoot: true,
-  routes: [
-    NativeStackRouter.of({
-      path: '/pages',
-      routes: [
-        Route.of<Pages.Page1>({
-          path: '/pages/page1',
-          build(router, route) {
-            return new Pages.Page1(router, route);
-          }
-        }),
-        Route.of<Pages.Page2>({
-          path: '/pages/page2',
-          build(router, route) {
-            return new Pages.Page2(router, route);
-          },
-        }),
-        NativeStackRouter.of({
-          path: '/pages/page3',
-          to: '/pages/page3/main',
-          modal: true,
-          routes: [
-            Route.of<Pages.Page3>({
-              path: '/pages/page3/main',
-              build(router, route) {
-                return new Pages.Page3(router, route);
-              }
-            })
-          ]
-        })
-      ]
-    })
-  ]
+  routes: [TabbarRoute]
 });
 
 export default router;
+
--- scripts/start.ts
+++ scripts/start.ts
@@ -2,4 +2,4 @@ import '@smartface/extension-utils';
 import 'theme';
 import router from 'routes';
 
-router.push('/pages/page1');
+router.push('/btb');
